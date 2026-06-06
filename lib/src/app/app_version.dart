b98a5511fb876358ae6c5500c59b689ea4e8f8e1
Fouad Khatib <42405091+Fouadktb@users.noreply.github.com>
2026-06-06 18:16:30 +0200

    chore: release v0.4.6

--- lib/src/app/app_version.dart
+++ lib/src/app/app_version.dart
@@ -1,5 +1,5 @@
-const velaVersion = '0.4.5';
-const velaBuildNumber = '11';
+const velaVersion = '0.4.6';
+const velaBuildNumber = '12';
 const velaReleaseTag = 'v$velaVersion';
 const velaRepositoryOwner = 'Fouadktb';
 const velaRepositoryName = 'vela';
